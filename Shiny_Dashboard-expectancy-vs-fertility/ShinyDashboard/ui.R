4e66da99206999dc233100cbfe6da64f97d8f242
Marina <marina.s.millan@gmail.com>
2022-02-10 12:36:59 -0500

    ui

--- Shiny_Dashboard-expectancy-vs-fertility/ShinyDashboard/ui.R
+++ Shiny_Dashboard-expectancy-vs-fertility/ShinyDashboard/ui.R
@@ -1,3 +1,4 @@
+library(plotly)
 library(shinydashboard)
 library(shiny)
 
@@ -8,13 +9,10 @@ dashboardPage(
     dashboardHeader(title = "Life expectancy vs. Fertility Around The World"),
     
     dashboardSidebar(
-        #sliderInput("year", "Select year ", min=1990, max=2018, value=2000),
-        #selectInput("region", "Select a Region", c(1960:2020), selected=2014),
-        #selectInput("country", "Select a Country", c(1960:2020), selected=2014),
         uiOutput("years"),
         uiOutput("regions"),
         uiOutput("countries")
-         ),
+    ),
     
     dashboardBody(
         tabPanel(title="My Plot",
